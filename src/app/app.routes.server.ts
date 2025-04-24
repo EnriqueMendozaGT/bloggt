616628d5bd16fa3c499405bb01ee81f826eb17fb
EnriqueMendoza <enriquemendoza0gt@gmail.com>
2025-04-23 22:55:14 -0600

    ready

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,28 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { routes } from './app.routes';
 
+// Mapear todas las rutas estáticas para prerender
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'noticias/:id',
-    renderMode: RenderMode.Client
+    path: '',
+    renderMode: RenderMode.Prerender,
   },
   {
+    path: 'noticias',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'ultimas-noticias',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    // Para rutas con parámetros usamos renderizado en el cliente
+    path: 'noticias/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    // Ruta por defecto
     path: '**',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Client,
   }
 ];
